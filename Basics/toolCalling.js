334b048da3d58948a06c7e0d9e6f26564d681e0d
Ahmad Raza <razarajpoot99598@gmail.com>
2026-09-19 01:05:23 +0500

    Built small chatbot

--- Basics/toolCalling.js
+++ Basics/toolCalling.js
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import {tavily} from '@tavily/core'
+import readline from 'readline/promises'
 
 import Groq from "groq-sdk";
 
@@ -10,119 +11,101 @@ const groq = new Groq({
 });
 
 async function main() {
-  
+  const rl = readline.createInterface({input:process.stdin, output:process.stdout})
   const messages = [
       {
         role: "system",
-        content: `You are smart assist. Respond to the user question and use tools if needed to answer the query.
+        content: `You are smart assist. Respond to the user question 
+                  and use tools if needed to answer the query.
                 
                 `,
       },
 
-      {
-        role: "user",
-        content: "Launched date of iphone 16? and how are you?",
-      },
+      // {
+      //   role: "user",
+      //   content: "What is the current weather of Lahore?",
+      // },
     ]
 
-  const completion = await groq.chat.completions.create({
-    model: "openai/gpt-oss-20b",
-    
-    temperature: 0,
-    messages: messages,
+  while(true){
 
-    tools: [
-      {
-        type: "function",
-        function: {
-          name: "webSearch",
-          description: "Search from web for latest news and information",
-          parameters: {
-            // JSON Schema object
-            type: "object",
-            properties: {
-              query: {
-                type: "string",
-                description: "Your query, you want to search from web",
+    const question = await rl.question("You: ")
+
+    if(question==="end") break;
+
+    messages.push({
+      role:"user",
+      content:question,
+    }) 
+      while(true){
+
+      const completion = await groq.chat.completions.create({
+      model: "openai/gpt-oss-20b",
+      
+      temperature: 0,
+      messages: messages,
+
+      tools: [
+        {
+          type: "function",
+          function: {
+            name: "webSearch",
+            description: "Search from web for latest news and information",
+            parameters: {
+              // JSON Schema object
+              type: "object",
+              properties: {
+                query: {
+                  type: "string",
+                  description: "Your query, you want to search from web",
+                },
+                
               },
-              
+              required: ["query"],
             },
-            required: ["query"],
           },
         },
-      },
-    ],
+      ],
 
-    tool_choice:"auto"
-  });
+      tool_choice:"auto"
+    });
 
-  const toolCalls = completion.choices[0].message.tool_calls
+    const toolCalls = completion.choices[0].message.tool_calls
 
-  if(toolCalls == null){
-    console.log(completion.choices[0].message.content);
-    return;
-  }
+    if(toolCalls == null){
+      console.log(completion.choices[0].message.content);
+      break;
+    }
 
-  for(const tool of toolCalls){
-        
-        const functionName = tool.function.name;
-        const functionParams = tool.function.arguments
-        
+    for(const tool of toolCalls){
+          
+          const functionName = tool.function.name;
+          const functionParams = tool.function.arguments
+          
 
-        if(functionName === 'webSearch'){
-           const toolResult = await webSearch(JSON.parse(functionParams))
+          if(functionName === 'webSearch'){
+              const toolResult = await webSearch(JSON.parse(functionParams))
 
-           messages.push({
-            tool_call_id: tool.id,
-            role:"tool",
-            name:functionName,
-            content:toolResult
-        })
-        }
-  }
+              messages.push({
+                tool_call_id: tool.id,
+                role:"tool",
+                name:functionName,
+                content:toolResult
+            })
+          }
+    }
 
-
-  const completion2 = await groq.chat.completions.create({
-    model: "openai/gpt-oss-20b",
-    
-    temperature: 0,
-    messages: messages,
-
-    tools: [
-      {
-        type: "function",
-        function: {
-          name: "webSearch",
-          description: "Search from web for latest news and information",
-          parameters: {
-            // JSON Schema object
-            type: "object",
-            properties: {
-              query: {
-                type: "string",
-                description: "Your query, you want to search from web",
-              },
-              
-            },
-            required: ["query"],
-          },
-        },
-      },
-    ],
-
-    tool_choice:"auto"
-  });
-
-
-  console.log(completion2.choices[0].message.content)
-
+    }
+  }  
+  
+  rl.close();
 }
 
 main();
 
 async function webSearch({ query }) {
-  const response = await tavly.search(query);
-  
+  const response = await tavly.search(query, {maxResults:2});
+  console.log("tool calling...")
   const searchResult = response.results.map((res)=>res.content).join("\n\n") 
   
   return searchResult;
